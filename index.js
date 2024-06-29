9f2a62ed391aa9af7bbd224fe1b5a1ba5ac61ab1
Ivylucyawuor <awuorivy09>
2024-06-29 21:10:25 +0300

    arrows part one

---
+++ index.js
@@ -0,0 +1,16 @@
+// Define a function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Define an arrow function called square
+  const square = x => x * x;
+  
+  // Define an arrow function called add
+  const add = (a, b) => a + b;
+  
+  // Test cases to verify the implementations
+  console.log(divide()); // 20
+  console.log(square(2)); // 4
+  console.log(add(3, 4)); // 7
+  
